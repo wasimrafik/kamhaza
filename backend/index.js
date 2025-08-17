63b2c111cf805a29e4f3411dda9dd0901f9ad372
wasimrafik <74105847+wasimrafik@users.noreply.github.com>
2025-08-17 18:51:10 +0530

    Revert "backend done"
    This reverts commit 84976ab8508ddfe4e32e6d51910fa42eac79578e.

--- backend/index.js
+++ backend/index.js
@@ -4,39 +4,14 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
-import { Server } from "socket.io";
-import http from "http";
-
 
 const app = express()
 
-const server = http.createServer(app);
+const PORT = process.env.PORT || 8001
 
-const io = new Server(server, {
-    cors: {
-        origin: process.env.CORS_ORIGIN,
-        methods: ["GET", "POST", "PUT", "DELETE"]
-    }
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
-
-// Log when a client connects or disconnects
-io.on("connection", (socket) => {
-    console.log("Client connected: " + socket.id);
-  
-    socket.on("disconnect", () => {
-      console.log("Client disconnected: " + socket.id);
-    });
-  });
-
-  // Make Socket.io instance accessible in your controllers via app.locals or similar
-app.set("socketio", io);
-
-
-// Start the server
-const PORT = process.env.PORT || 8001;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 mongoose
@@ -57,18 +32,6 @@ app.use(express.static('public'))
 
 import userRouter from './router/user.router.js'
 import productRouter from './router/product.router.js'
-import categoryRouter from './router/category.router.js'
-import messageRouter from './router/message.router.js'
-import addToBookmarkRouter from './router/addToBookmark.router.js'
-import locationRouter from './router/location.router.js'
-import notificationRouter from './router/notification.router.js'
-import transactionRouter from './router/transaction.router.js'
 
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
-app.use('/api/category', categoryRouter)
-app.use('/api/message', messageRouter)
-app.use('/api/bookmark', addToBookmarkRouter)
-app.use('/api/location', locationRouter)
-app.use('/api/notification', notificationRouter)
-app.use('/api/transaction', transactionRouter)
